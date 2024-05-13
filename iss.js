11e5ca010c9c25525ce0014c317a360737a0f519
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-12 18:23:33 -0700

    Update iss.js

--- iss.js
+++ iss.js
@@ -6,8 +6,14 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
+
+const request = require('request');
+
 const fetchMyIP = function(callback) { 
   // use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    // handle error and response here
+  });
 }
 
 module.exports = { fetchMyIP };
