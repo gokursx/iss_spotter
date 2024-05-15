60d4b36b62b86647c8902e6bcf2ef7439918cf56
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-15 14:44:24 -0700

    Update iss.js

--- iss.js
+++ iss.js
@@ -11,8 +11,22 @@ const request = require('request');
 
 const fetchMyIP = function(callback) { 
   // use request to fetch IP address from JSON API
-  request('https://api.ipify.org?format=json', (error, response, body) => {
+  request('https://api.ipify.org?format=json', (error, data) => {
     // handle error and response here
+      // inside the request callback ...
+  // error can be set if invalid domain, user is offline, etc.
+  if (error) {
+    callback(error, null);
+    return;
+  }
+  // if non-200 status, assume server error
+  if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
+
+  // if we get here, all's well and we got the data
   });
 }
 
@@ -26,6 +40,12 @@ const fetchCoordsByIP = function(ip, callback) {
       callback(error, null);
       return;
     }
+    // if non-200 status, assume server error
+  if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
   const parsedBody = JSON.parse(data);
     const latLong = {lat: parsedBody.latitude, long: parsedBody.longitude};
     callback(null, latLong);
