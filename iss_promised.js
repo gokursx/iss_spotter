40255bbc5b17af19388604b110244396df516c31
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-15 15:14:38 -0700

    Update iss_promised.js

--- iss_promised.js
+++ iss_promised.js
@@ -1,2 +1,26 @@
 // iss_promised.js
+
+//Importing needle function
 const needle = require('needle');
+
+/* 
+ * Makes a request to ipwho.is using the provided IP address to get its geographical information (latitude/longitude)
+ * Input: IP address as a string
+ * Returns: Promise of request for lat/lon
+ */
+
+const fetchMyIP = function() {
+  return needle.get('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(ip) {
+  return needle.get(geoLocationUrl);
+};
+
+const fetchISSFlyOverTimes = function(coords) {
+  const flyoverUrl = ``;
+  return needle.get(flyoverUrl);
+};
+
+// export needed modules
+module.exports = { fetchMyIP: fetchMyIP, fetchCoordsByIP: fetchCoordsByIP, fetchISSFlyOverTimes: fetchISSFlyOverTimes };
